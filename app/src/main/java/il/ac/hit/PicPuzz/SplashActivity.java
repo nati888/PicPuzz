db0bf20d9a0a35323c20772462f38ccab0c54612
Erez Mizrahi <erezmiz@my.hit.ac.il>
2021-02-08 16:26:28 +0200

    added splash screen

---
+++ app/src/main/java/il/ac/hit/PicPuzz/SplashActivity.java
@@ -0,0 +1,46 @@
+package il.ac.hit.picpuzz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    public static int SPLASH_SCREEN = 2000;
+    Animation topAnim, botAnim;
+    ImageView image;
+    TextView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        botAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.img_logo);
+        logo = findViewById(R.id.name_logo);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(botAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+}
